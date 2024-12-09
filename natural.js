9e070191906f37d51113d167f3228b1455c92aa3
Debo Valinotte <debovalinotte@gmail.com>
2024-12-09 14:37:17 -0300

    proyecto final

---
+++ natural.js
@@ -0,0 +1,8 @@
+function cargarEquipo()
+{//document.getElementById('equipo').innerHTML=
+ var pestaña1 = document.getElementById('equipo')
+
+}
+function cargarProcedimiento(){
+    document.getElementById('procedimientos').innerHTML=
+}
